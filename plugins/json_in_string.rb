3109b4b17b8c568603f47f11e35e47dd517da7d3
Richard Bolkey <rick@onespot.com>
2016-09-19 22:43:37 -0500

    Updating based on 1.0.0 of example plugin.

--- plugins/json_in_string.rb
+++ plugins/json_in_string.rb
@@ -17,13 +17,13 @@ module Fluent
             # This is the main method. The input "text" is the unit of data to be parsed.
             # If this is the in_tail plugin, it would be a line. If this is for in_syslog,
             # it is a single syslog message.
-            def parse(text)
+            def parse(input)
                 begin
-                    hash = JSON.parse(text)
-                    time = @time_parser.parse(hash[@time_key])
+                    output = (input.is_a?(Hash)) ? (input) : (JSON.parse(input))
+                    time = @time_parser.parse(output[@time_key])
                     yield time, hash
                 rescue
-                    yield text
+                    yield input
                 end
             end
         end
